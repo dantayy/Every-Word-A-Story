8ee7f1478aafb31b98fd50f675659a33edd07178
Nick Mercadante (RIT Student) <nvm6501@ad.rit.edu>
2019-11-08 13:55:17 -0500

    Began making derivatives of the domo functions for the words

--- client/app/maker.js
+++ client/app/maker.js
@@ -1,21 +1,8 @@
-const handleDomo = (e) => {
-    e.preventDefault();
-    
-    $(`#domoMessage`).animate({width:`hide`}, 350);
-    
-    if($(`#domoName`).val() === `` || $(`#domoAge`).val() === ``) {
-        handleError(`RAWR!  All fields are required`);
-        return false;
-    }
-    
-    sendAjax(`POST`, $(`#domoForm`).attr(`action`), $(`#domoForm`).serialize(), function() {
-        loadDomosFromServer();
-    });
-    
-    return false;
-};
+// users can't make words longer than this
+const maxWordLength = 30;
 
-const handleView = (e) => {
+
+const handleDomoView = (e) => {
     if(e.target.value === "all"){
         loadAllDomosFromServer();
     } else {
@@ -23,17 +10,67 @@ const handleView = (e) => {
     }
 };
 
+//change the display from all words to user's words & vice versa
+const handleWordView = (e) => {
+    if(e.target.value === "all"){
+        loadAllWordsFromServer();
+    } else {
+        loadWordsFromServer();
+    }
+};
+
+const handleDomo = (e) => {
+    e.preventDefault();
+
+    $(`#domoMessage`).animate({width:`hide`}, 350);
+
+    if($(`#domoName`).val() === `` || $(`#domoAge`).val() === ``) {
+        handleError(`RAWR!  All fields are required`);
+        return false;
+    }
+
+    sendAjax(`POST`, $(`#domoForm`).attr(`action`), $(`#domoForm`).serialize(), function() {
+        loadDomosFromServer();
+    });
+
+    return false;
+};
+
+// func for handling a string the user wants to add to the story
+const handleWord = (e) => {
+    e.preventDefault();
+
+    $(`#domoMessage`).animate({width:`hide`}, 350);
+
+    if($(`#word`).val() === ``) {
+        handleError(`RAWR!  Need to put in a word!`);
+        return false;
+    } else if ($(`#word`).length() > maxWordLength) {
+        handleError(`RAWR!  Word is too long!`);
+        return false;
+    } else if ($(`#word`).index(' ') !== -1) {
+        handleError(`RAWR!  Can't put in multiple words!`);
+        return false;
+    }
+
+    sendAjax(`POST`, $(`#wordForm`).attr(`action`), $(`#wordForm`).serialize(), () => {
+        loadAllWordsFromServer();
+    });
+
+    return false;
+};
+
 const DomoForm = (props) => {
     return (
         <form id="domoForm" onSubmit={handleDomo} name="domoForm" action="/maker" method="POST" className="domoForm">
-            <label htmlFor="name">Name: </label>
-            <input id="domoName" type="text" name="name" placeholder="Domo Name" />
-            <label htmlFor="age">Age: </label>
-            <input id="domoAge" type="text" name="age" placeholder="Domo Age" />
-            <label htmlFor="image">Image: </label>
-            <input id="domoImage" type="text" name="image" placeholder="Domo Image URL" />
-            <input type="hidden" name="_csrf" value={props.csrf} />
-            <input className="makeDomoSubmit" type="submit" value="Make Domo" />
+        <label htmlFor="name">Name: </label>
+        <input id="domoName" type="text" name="name" placeholder="Domo Name" />
+        <label htmlFor="age">Age: </label>
+        <input id="domoAge" type="text" name="age" placeholder="Domo Age" />
+        <label htmlFor="image">Image: </label>
+        <input id="domoImage" type="text" name="image" placeholder="Domo Image URL" />
+        <input type="hidden" name="_csrf" value={props.csrf} />
+        <input className="makeDomoSubmit" type="submit" value="Make Domo" />
         </form>
     );
 };
@@ -42,24 +79,46 @@ const DomoList = (props) => {
     if(props.domos.length === 0) {
         return(
             <div className="domoList">
-                <h3 className="emptyDomo">No Domos yet</h3>
+            <h3 className="emptyDomo">No Domos yet</h3>
             </div>
         );
     }
-    
+
     const domoNodes = props.domos.map((domo) => {
         return (
             <div key={domo._id} className="domo">
-                <img src={domo.image} alt="domo face" className="domoFace" />
-                <h3 className="domoName">Name: {domo.name}</h3>
-                <h3 className="domoAge">Age: {domo.age}</h3>
+            <img src={domo.image} alt="domo face" className="domoFace" />
+            <h3 className="domoName">Name: {domo.name}</h3>
+            <h3 className="domoAge">Age: {domo.age}</h3>
             </div>
         );
     });
-    
+
     return(
         <div className="domoList">
-            {domoNodes}
+        {domoNodes}
+        </div>
+    );
+};
+
+const WordList = (props) => {
+    if(props.words.length === 0) {
+        return(
+            <div className="domoList">
+            <h3 className="emptyDomo">No words yet</h3>
+            </div>
+        );
+    }
+
+    const wordNodes = props.words.map((word) => {
+        return (
+            <span key={word._id} color={word.color}>{word.text} </span>
+        );
+    });
+
+    return(
+        <div className="domoList">
+        {wordNodes}
         </div>
     );
 };
@@ -67,10 +126,21 @@ const DomoList = (props) => {
 const DomoView = (props) => {
     return (
         <form id="domoView" name="domoView" className="domoForm">
-            <label htmlFor="view">View All Domos - </label>
-            <input type="radio" onClick={handleView} name="view" value="all" />
-            <label htmlFor="view">View Your Domos - </label>
-            <input type="radio" onClick={handleView} name="view" value="user" defaultChecked />
+        <label htmlFor="view">View All Domos - </label>
+        <input type="radio" onClick={handleDomoView} name="view" value="all" />
+        <label htmlFor="view">View Your Domos - </label>
+        <input type="radio" onClick={handleDomoView} name="view" value="user" defaultChecked />
+        </form>
+    );
+};
+
+const WordView = (props) => {
+    return (
+        <form id="domoView" name="wordView" className="domoForm">
+        <label htmlFor="view">View All Words - </label>
+        <input type="radio" onClick={handleWordView} name="view" value="all" />
+        <label htmlFor="view">View Your Words - </label>
+        <input type="radio" onClick={handleWordView} name="view" value="user" defaultChecked />
         </form>
     );
 };
@@ -84,6 +154,15 @@ const loadDomosFromServer = () => {
     });
 };
 
+const loadWordsFromServer = () => {
+    sendAjax(`GET`, `/getWords`, null, (data) => {
+        ReactDOM.render(
+            <WordList words={data.words} />,
+            document.querySelector(`#words`)
+        );
+    });
+};
+
 const loadAllDomosFromServer = () => {
     sendAjax(`GET`, `/getAllDomos`, null, (data) => {
         ReactDOM.render(
@@ -93,22 +172,31 @@ const loadAllDomosFromServer = () => {
     });
 };
 
+const loadAllWordsFromServer = () => {
+    sendAjax(`GET`, `/getAllWords`, null, (data) => {
+        ReactDOM.render(
+            <WordList words={data.words} />,
+            document.querySelector(`#words`)
+        );
+    });
+};
+
 const setup = (csrf) => {
     ReactDOM.render(
         <DomoForm csrf={csrf} />,
         document.querySelector(`#makeDomo`)
     );
-    
+
     ReactDOM.render(
         <DomoView />,
         document.querySelector(`#domoView`)
     );
-    
+
     ReactDOM.render(
         <DomoList domos={[]} />,
         document.querySelector(`#domos`)
     );
-    
+
     loadDomosFromServer();
 };
 
