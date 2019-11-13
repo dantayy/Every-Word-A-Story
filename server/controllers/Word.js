deb039fa9c24d16786bb10d6800dccb174bbc79a
dantayy <nvm6501@rit.edu>
2019-11-12 21:32:30 -0500

    fixed minor eslint error

--- server/controllers/Word.js
+++ server/controllers/Word.js
@@ -6,73 +6,74 @@ const Word = models.Word;
 
 // renders the default app page with all words loaded
 const makerPage = (req, res) => {
-    Word.WordModel.find({}, (err, docs) => {
-        if (err) {
-            console.log(err);
-            return res.status(400).json({ error: 'An error occurred' });
-        }
+  Word.WordModel.find({}, (err, docs) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: 'An error occurred' });
+    }
 
-        return res.render('app', { csrfToken: req.csrfToken(), words: docs });
-    });
+    return res.render('app', { csrfToken: req.csrfToken(), words: docs });
+  });
 };
 
 // make a new word based on the text parsed and account signed in
 const makeWord = (req, res) => {
-    console.log(`Old lastPosted time: ${Date.parse(req.session.account.lastPosted)}`);
+  console.log(`Old lastPosted time: ${Date.parse(req.session.account.lastPosted)}`);
 
-    // check to see if no word was submitted or if timeout hasn't ended yet
-    if (!req.body.text) {
-        return res.status(400).json({ error: 'All fields are required' });
-    } else if (Date.now() - req.session.account.timeBetweenPosts < Date.parse(req.session.account.lastPosted)) {
-        return res.status(400).json({ error: 'Your post timeout isn\'t over yet, please wait' });
+  // check to see if no word was submitted or if timeout hasn't ended yet
+  if (!req.body.text) {
+    return res.status(400).json({ error: 'All fields are required' });
+  } else if (Date.now() - req.session.account.timeBetweenPosts <
+             Date.parse(req.session.account.lastPosted)) {
+    return res.status(400).json({ error: 'Your post timeout isn\'t over yet, please wait' });
+  }
+
+  // build a new word with data provided
+  const wordData = {
+    text: req.body.text,
+    owner: req.session.account._id,
+    color: req.session.account.color,
+  };
+  const newWord = new Word.WordModel(wordData);
+  const wordPromise = newWord.save();
+  // update the account's lastPosted property and reload the page after successful submission
+  wordPromise.then(() => {
+    models.Account.AccountModel.updateLastPosted(req.session.account.username, Date.now());
+    console.log(`New lastPosted time: ${Date.parse(req.session.account.lastPosted)}`);
+    res.json({ redirect: '/maker' });
+  });
+  // catch errors
+  wordPromise.catch((err) => {
+    console.log(err);
+
+    if (err.code === 11000) {
+      return res.status(400).json({ error: 'Word already exists' });
     }
 
-    // build a new word with data provided
-    const wordData = {
-        text: req.body.text,
-        owner: req.session.account._id,
-        color: req.session.account.color,
-    };
-    const newWord = new Word.WordModel(wordData);
-    const wordPromise = newWord.save();
-    // update the account's lastPosted property and reload the page after successful submission
-    wordPromise.then(() => {
-        models.Account.AccountModel.updateLastPosted(req.session.account.username, Date.now());
-        console.log(`New lastPosted time: ${Date.parse(req.session.account.lastPosted)}`);
-        res.json({ redirect: '/maker' });
-    });
-    // catch errors
-    wordPromise.catch((err) => {
-        console.log(err);
-
-        if (err.code === 11000) {
-            return res.status(400).json({ error: 'Word already exists' });
-        }
-
-        return res.status(400).json({ error: 'An error occurred' });
-    });
-    return wordPromise;
+    return res.status(400).json({ error: 'An error occurred' });
+  });
+  return wordPromise;
 };
 
 // get words made by the user currently signed in
 const getWords = (req, res) => Word.WordModel.findByOwner(req.session.account._id, (err, docs) => {
-    if (err) {
-        console.log(err);
-        return res.status(400).json({ error: 'An error occurred' });
-    }
+  if (err) {
+    console.log(err);
+    return res.status(400).json({ error: 'An error occurred' });
+  }
 
-    return res.json({ words: docs });
+  return res.json({ words: docs });
 });
 
 
 // get words made by all users
 const getAllWords = (req, res) => Word.WordModel.find({}, (err, docs) => {
-    if (err) {
-        console.log(err);
-        return res.status(400).json({ error: 'An error occurred' });
-    }
+  if (err) {
+    console.log(err);
+    return res.status(400).json({ error: 'An error occurred' });
+  }
 
-    return res.json({ words: docs });
+  return res.json({ words: docs });
 });
 
 // export relevant functions for the router
