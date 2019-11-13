28c8351dbd58c35c683dc0bfdf811e265dac29ff
dantayy <nvm6501@rit.edu>
2019-11-12 21:20:51 -0500

    Began reformating the main app page to utilize bootstrap

--- server/models/Account.js
+++ server/models/Account.js
@@ -9,96 +9,102 @@ const saltLength = 64;
 const keyLength = 64;
 
 const AccountSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-    match: /^[A-Za-z0-9_\-.]{1,16}$/,
-  },
-  salt: {
-    type: Buffer,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  createdDate: {
-    type: Date,
-    default: Date.now,
-  },
-  color: {
-    type: String,
-    required: true,
-    default: 'black',
-  },
-  timeBetweenPosts: {
-    type: Number,
-    required: true,
-  },
-  lastPosted: {
-    type: Date,
-    required: true,
-  },
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        match: /^[A-Za-z0-9_\-.]{1,16}$/,
+    },
+    salt: {
+        type: Buffer,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    createdDate: {
+        type: Date,
+        default: Date.now,
+    },
+    color: {
+        type: String,
+        required: true,
+        default: 'black',
+    },
+    timeBetweenPosts: {
+        type: Number,
+        required: true,
+    },
+    lastPosted: {
+        type: Date,
+        required: true,
+    },
 });
 
 AccountSchema.statics.toAPI = doc => ({
-  // _id is built into your mongo document and is guaranteed to be unique
-  username: doc.username,
-  _id: doc._id,
-  color: doc.color,
-  timeBetweenPosts: doc.timeBetweenPosts,
-  lastPosted: doc.lastPosted,
+    // _id is built into your mongo document and is guaranteed to be unique
+    username: doc.username,
+    _id: doc._id,
+    color: doc.color,
+    timeBetweenPosts: doc.timeBetweenPosts,
+    lastPosted: doc.lastPosted,
 });
 
 const validatePassword = (doc, password, callback) => {
-  const pass = doc.password;
+    const pass = doc.password;
 
-  return crypto.pbkdf2(password, doc.salt, iterations, keyLength, 'RSA-SHA512', (err, hash) => {
-    if (hash.toString('hex') !== pass) {
-      return callback(false);
-    }
-    return callback(true);
-  });
+    return crypto.pbkdf2(password, doc.salt, iterations, keyLength, 'RSA-SHA512', (err, hash) => {
+        if (hash.toString('hex') !== pass) {
+            return callback(false);
+        }
+        return callback(true);
+    });
 };
 
 AccountSchema.statics.findByUsername = (name, callback) => {
-  const search = {
-    username: name,
-  };
+    const search = {
+        username: name,
+    };
 
-  return AccountModel.findOne(search, callback);
+    return AccountModel.findOne(search, callback);
 };
 
 AccountSchema.statics.generateHash = (password, callback) => {
-  const salt = crypto.randomBytes(saltLength);
+    const salt = crypto.randomBytes(saltLength);
 
-  crypto.pbkdf2(password, salt, iterations, keyLength, 'RSA-SHA512', (err, hash) =>
-    callback(salt, hash.toString('hex'))
-  );
+    crypto.pbkdf2(password, salt, iterations, keyLength, 'RSA-SHA512', (err, hash) =>
+                  callback(salt, hash.toString('hex'))
+                 );
 };
 
 AccountSchema.statics.authenticate = (username, password, callback) =>
-  AccountModel.findByUsername(username, (err, doc) => {
+AccountModel.findByUsername(username, (err, doc) => {
     if (err) {
-      return callback(err);
+        return callback(err);
     }
 
     if (!doc) {
-      return callback();
+        return callback();
     }
 
     return validatePassword(doc, password, (result) => {
-      if (result === true) {
-        return callback(null, doc);
-      }
+        if (result === true) {
+            return callback(null, doc);
+        }
 
-      return callback();
+        return callback();
     });
-  });
+});
 
-AccountSchema.statics.updateLastPosted = (id, date) => AccountModel.findOneAndUpdate(id, date);
+AccountSchema.statics.updateLastPosted = (un, date) => {
+    console.log(`username of acct being updated: ${un}`);
+    console.log(`date (in ms) passed through to update the lastPosted property: ${date}`);
+    AccountModel.updateOne({ username: un, }, {
+        lastPosted: date,
+    });
+};
 
 AccountModel = mongoose.model('Account', AccountSchema);
 
